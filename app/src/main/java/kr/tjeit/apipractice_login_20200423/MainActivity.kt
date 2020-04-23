93a85477464d7c9cbd7bca6baf527775e44f7846
cho881020 <kj_cho@idealidea.co.kr>
2020-04-23 21:51:00 +0900

    로그인화면구현 / 이벤트 처리 기초

--- app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
@@ -2,15 +2,26 @@ package kr.tjeit.apipractice_login_20200423
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString() // cho881020 => 조경진
+            val inputPw = pwEdt.text.toString() // asdf1234
+
+//            로그인?
+
+        }
 
     }
 
