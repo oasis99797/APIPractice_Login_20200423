0e8b10f056cb483d161ab7339ad08e0acb980a48
cho881020 <kj_cho@idealidea.co.kr>
2020-04-23 21:15:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
@@ -0,0 +1,21 @@
+package kr.tjeit.apipractice_login_20200423
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
