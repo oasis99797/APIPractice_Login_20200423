f442198973b99f14b93e282605441d68c7cc1306
cho881020 <kj_cho@idealidea.co.kr>
2020-04-27 21:03:00 +0900

    메인화면 추가 => 로그인 한 사람 이름 출력 준비  - 기존의 메인액티비티를 로그인액티비티로 이름 변경

---
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/LoginActivity.kt
@@ -0,0 +1,59 @@
+package kr.tjeit.apipractice_login_20200423
+
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import kr.tjeit.apipractice_login_20200423.utils.ServerUtil
+import org.json.JSONObject
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString() // cho881020 => 조경진
+            val inputPw = pwEdt.text.toString() // asdf1234
+
+//            서버로 로그인 요청 => ServerUtil클래스 기능 활용
+            ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+//                    실제로 응답을 받은걸 분석해서 => 대응
+
+//                    임시로 서버 응답 확인 하기 위한 코드
+//                    Log.d("서버응답JSON", json.toString())
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+//                        로그인 성공
+                    }
+                    else {
+                        val message = json.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                }
+
+            })
+
+        }
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
--- app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
@@ -2,11 +2,6 @@ package kr.tjeit.apipractice_login_20200423
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import kr.tjeit.apipractice_login_20200423.utils.ServerUtil
-import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -18,39 +13,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-
-        loginBtn.setOnClickListener {
-            val inputId = idEdt.text.toString() // cho881020 => 조경진
-            val inputPw = pwEdt.text.toString() // asdf1234
-
-//            서버로 로그인 요청 => ServerUtil클래스 기능 활용
-            ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler {
-                override fun onResponse(json: JSONObject) {
-
-//                    실제로 응답을 받은걸 분석해서 => 대응
-
-//                    임시로 서버 응답 확인 하기 위한 코드
-//                    Log.d("서버응답JSON", json.toString())
-
-                    val code = json.getInt("code")
-
-                    if (code == 200) {
-//                        로그인 성공
-                    }
-                    else {
-                        val message = json.getString("message")
-
-                        runOnUiThread {
-                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
-                        }
-
-                    }
-
-                }
-
-            })
-
-        }
 
     }
 
