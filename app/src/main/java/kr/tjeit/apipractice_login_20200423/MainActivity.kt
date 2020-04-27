0e70bedd24429453a89272aa3bf59bb40656ccb3
cho881020 <kj_cho@idealidea.co.kr>
2020-04-27 20:16:32 +0900

    로그인버튼 -> ID / PW가지고 서버에 로그인 요청 -> 응답 로그로 확인

--- app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/MainActivity.kt
@@ -2,7 +2,10 @@ package kr.tjeit.apipractice_login_20200423
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.tjeit.apipractice_login_20200423.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -19,7 +22,19 @@ class MainActivity : BaseActivity() {
             val inputId = idEdt.text.toString() // cho881020 => 조경진
             val inputPw = pwEdt.text.toString() // asdf1234
 
-//            로그인?
+//            서버로 로그인 요청 => ServerUtil클래스 기능 활용
+            ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+//                    실제로 응답을 받은걸 분석해서 => 대응
+
+//                    임시로 서버 응답 확인 하기 위한 코드
+                    Log.d("서버응답JSON", json.toString())
+
+
+                }
+
+            })
 
         }
 
--- app/src/main/java/kr/tjeit/apipractice_login_20200423/utils/ServerUtil.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/utils/ServerUtil.kt
@@ -16,7 +16,8 @@ class ServerUtil {
 //        어느 서버(호스트)로 가야하는지 적어두는 변수.
 //        도메인 (api.naver.com 등) or IP주소 (192.168.0.243:5000) 등
 //        메인 주소를 저장해두는 변수.
-        val BASE_URL = "http://192.168.0.243:5000"
+//        val BASE_URL = "http://api.tjeit.com" // 라이브주소
+        val BASE_URL = "http://192.168.10.224:5000" // 개발용주소
 
 //        필요한 변수를 넣는 요령.
 //        화면에서 => 서버로 전달해야하는 데이터.
@@ -33,13 +34,17 @@ class ServerUtil {
                 .add("password", pw)
                 .build()
 
+
+//            화면이동으로 따지면 Intent 객체를 만드는 행위.
             val request = Request.Builder()
                 .url(urlStr)
                 .post(formBody)
 //                .header()  => API가 헤더를 요구하면 추가해야함.
                 .build()
 
+//            startActivity 처럼 실제로 요청을 날리는 코드.
             client.newCall(request).enqueue(object : Callback {
+
                 override fun onFailure(call: Call, e: IOException) {
 //                    서버 연결 자체를 실패.
 
@@ -52,7 +57,7 @@ class ServerUtil {
 //                    서버의 응답은 보통 JSON양식으로 가공되어 옴.
 //                    받을떄는 일단 String타입으로 받게됨. => JSON으로 변환해서 액티비티에 전달.
 
-                    val body = response.body!!.toString()
+                    val body = response.body!!.string()
                     val json = JSONObject(body)
 
                     handler?.onResponse(json)
